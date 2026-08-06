aa9bb6597d8b37a068771f120e2ee8b8dfc7e177
simhadri <simhadri1998@gmail.com>
2026-08-06 02:50:37 +0200

    feat(zsh): enable and configure zsh
    - Enable zsh with autosuggestions and syntax highlighting.
    - Add common shell aliases for navigation, package management, and listing.
    - Bind '^f' to accept autosuggestions.

--- home.nix
+++ home.nix
@@ -35,6 +35,28 @@ in
   fonts.fontconfig.enable = true;
 
   ######################################################################
+  ## shell configuration
+  ######################################################################
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;      # ghost text from history
+    syntaxHighlighting.enable = true;  # commands turn green when valid
+    initContent = ''
+      bindkey '^f' autosuggest-accept
+    '';
+    shellAliases = {
+      ".." = "cd ..";
+      uvup = "uv tool upgrade --all";
+      pn = "pnpm";
+      brewup = "brew update && brew upgrade";
+      ls = "eza --long --icons=auto";
+      lssz = "eza --long --reverse --sort=size";
+      lg = "lazygit";
+    };
+  };
+
+  ######################################################################
   ## Symlinks to config files in the repo
   ######################################################################
 
