d44902e0cfbe0493279aaedd74ebbb8b24fdcf6a
simhadri <simhadri1998@gmail.com>
2026-08-06 14:18:42 +0200

    chore(wezterm): increase background opacity and blur
    - opacity 0.8 to 0.9, blur 50 to 80
    - Add trailing newline at EOF

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -5,10 +5,11 @@ local config = wezterm.config_builder()
 config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 16.0
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 50
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 80
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 150
 config.initial_rows = 50
 
 return config
+
