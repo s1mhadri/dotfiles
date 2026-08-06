0459d41645a0760dd88f548ca22a0a81361a322e
simhadri <simhadri1998@gmail.com>
2026-08-06 02:56:53 +0200

    feat(shell): enable and configure starship prompt
    - Enable starship prompt in home.nix.
    - Configure prompt format and symbols for a cleaner look.

--- home.nix
+++ home.nix
@@ -57,6 +57,23 @@ in
   };
 
   ######################################################################
+  ## starship prompt configuration
+  ######################################################################
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+      };
+      cmd_duration.format = "[$duration]($style) ";
+    };
+  };
+
+  ######################################################################
   ## Symlinks to config files in the repo
   ######################################################################
 
