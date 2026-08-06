a7ee947fa7caeb338edda1bd19a5320310903507
simhadri <simhadri1998@gmail.com>
2026-08-06 03:45:46 +0200

    feat(editor): add navigation plugin configuration for neovim
    - Create navigation.lua in the plugins directory.
    - Configure navigation-related plugins for Neovim.

---
+++ home/.config/nvim/lua/plugins/navigation.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      input = { enabled = true },
+    },
+    keys = {
+      { '<leader>f', function() Snacks.picker.files() end, desc = 'Find Files' },
+      { '<leader>s', function() Snacks.picker.grep() end,  desc = 'Search Text' },
+      { '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+      { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
+    },
+  },
+}
+
