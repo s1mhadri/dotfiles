f253f14de1196edfaa3fc303cb8a22c8b2c74baf
simhadri <simhadri1998@gmail.com>
2026-08-06 02:23:14 +0200

    feat(home): add base packages and basic configuration
    - Add essential CLI tools (ripgrep, fd, fzf, lazygit, neovim, etc.)
    - Install Nerd Font Hack
    - Set nvim as the default editor
    - Enable fontconfig

--- home.nix
+++ home.nix
@@ -9,6 +9,35 @@ in
   home.homeDirectory = "/Users/${user}";
   home.stateVersion = "24.11";
 
+  home.packages = with pkgs; [
+    # cli i use constantly
+    ripgrep   # fast search
+    fd        # fast find
+    fzf       # fuzzy finder
+    lazygit
+    neovim
+    cmake
+    eza
+    ffmpeg
+    gh
+    gnupg
+    livekit
+    livekit-cli
+    pnpm
+    rbenv
+    uv
+    # fonts
+    nerd-fonts.hack
+  ];
+
+  home.sessionVariables.EDITOR = "nvim";
+
+  fonts.fontconfig.enable = true;
+
+  ######################################################################
+  ## Symlinks to config files in the repo
+  ######################################################################
+
   # Edit-in-place: the real file stays in the repo, ~/.config just points at it.
   home.file.".config/wezterm".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
